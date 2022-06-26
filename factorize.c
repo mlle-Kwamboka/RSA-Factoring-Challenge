4b2ede2b451ce91dff4385b68603dde1809dbacc
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-06-26 13:13:50 -0700

    RSA Challenge try

---
+++ factorize.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _factorize - factorizes a number
+ * @a: the number to factorize
+ *
+ * Return: Factors
+ */
+int _factorize(int a)
+{
+	int i, j;
+
+	for (i = 0; i <= y; i++)
+	{
+		if (j % 2 == 0 || j % 3 == 0);
+		{
+			printf ("%d = %d * %d", i
+		}
+	}
+}
